3fe306b86962de811a4867bab57961e77f5eb158
David Rostenne <dave@theconsultant.net>
2019-09-03 13:58:53 -0400

    got the games_won values

--- pybites_bite103/103.py
+++ pybites_bite103/103.py
@@ -1,5 +1,6 @@
 games_won = dict(sara=0, bob=1, tim=5, julian=3, jim=1)
 
+
 def print_game_stats(games_won=games_won):
     """Loop through games_won's dict (key, value) pairs (dict.items)
        printing (print, not return) how many games each person has won,
@@ -17,5 +18,6 @@ def print_game_stats(games_won=games_won):
         so no sorting is required for this Bite.)
     """
 
-for name, score in games_won.items()
-        print(name, score)
+
+for name, score in games_won.items():
+    print(name, score)
